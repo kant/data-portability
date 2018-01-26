3b718d6320833cf23809b6812b6c72baaa77d593
Jessie "Chuy" Chavez <chuy@google.com>
2018-01-26 11:26:07 -0600

    initial server shell

---
+++ portability-gateway/src/main/java/org/dataportabilityproject/gateway/ServerMain.java
@@ -0,0 +1,13 @@
+package org.dataportabilityproject.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/** Starts the reference api server. */
+public class ServerMain {
+    private static final Logger logger = LoggerFactory.getLogger(ServerMain.class);
+    public static void main(String args) {
+        logger.warn("Starting reference api server.");
+    }
+}
